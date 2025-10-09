9f394eb0258fdce44002e4b732094ad633ec85a8
Olivier Roussel <olivier.roussel@inria.fr>
2025-10-09 16:38:30 +0200

    debugging

--- scripts/filter_configs.py
+++ scripts/filter_configs.py
@@ -3,22 +3,25 @@ import sys
 
 # Usage: filter_config.py <config_to_filter.yaml> <filter_file.yaml>
 if __name__ == "__main__":
-  filename = sys.argv[1]
+  config_filename = sys.argv[1]
   filter_file = sys.argv[2]
+  print('config_filename = ', config_filename)
+  print('filter_file = ', filter_file)
 
   # read config file to filter
-  with open(filename, "r") as file:
+  with open(config_filename, "r") as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
 
   # read filter.yaml
   with open(filter_file, "r") as file:
     filt = yaml.full_load(file)
+    print('filt:', filt)
 
-    print("top-level attributes to be filtered config yaml: ", filt['filtered'])
+    # print("top-level attributes to be filtered config yaml: ", filt['filtered'])
     for l in filt['filtered']:
       print("filtering ", l, "...")
       config.pop(l, None)
 
   # output to file
-  with open(filename, "w") as file:
+  with open(config_filename, "w") as file:
     yaml.dump(config, file)
