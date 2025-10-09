33ea0d3bdcfa9314ec20b173057adfdee715b430
Olivier Roussel <olivier.roussel@inria.fr>
2025-10-09 16:17:59 +0200

    debugging

--- scripts/rerender.sh
+++ scripts/rerender.sh
@@ -11,13 +11,14 @@ export RATTLER_BUILD_EXPERIMENTAL=true
 CONDA_BUILD_CONFIG_FILE=$1
 FEEDSTOCK_DIR=$2
 
-echo "=== CONDA_BUILD_CONFIG_FILE = $CONDA_BUILD_CONFIG_FILE"
-echo "=== FEEDSTOCK_DIR = $FEEDSTOCK_DIR"
+echo "CONDA_BUILD_CONFIG_FILE = $CONDA_BUILD_CONFIG_FILE"
+echo "FEEDSTOCK_DIR = $FEEDSTOCK_DIR"
 
 # run conda smithy:
 # conda smithy rerender --feedstock_config conda/configs/conda-build.yaml --feedstock_directory <recipe_folder>
+
 conda smithy rerender --feedstock_config $CONDA_BUILD_CONFIG_FILE --feedstock_directory $FEEDSTOCK_DIR
 
 # filter <recipe_folder>/.ci_support/*.yaml files from unused conda-forge specific fields, i.e.: 
 # docker_image, channel_targets...
-# python scripts/filter_configs.py $FEEDSTOCK_DIR/.ci_support/*.yaml scripts/filter.yaml
+python scripts/filter_configs.py $FEEDSTOCK_DIR/.ci_support/*.yaml scripts/filter.yaml
