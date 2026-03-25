dfd3199c4a00eddf0ca8a61be0745d4dd5a9a495
Olivier Roussel <oroussel@laas.fr>
2026-03-25 14:41:27 +0100

    handle query packages returns none

--- .github/workflows/fetch_packages_names.py
+++ .github/workflows/fetch_packages_names.py
@@ -34,17 +34,19 @@ response.raise_for_status()
 
 resp_json = response.json()
 
-page = resp_json["data"]["package"]["variants"]["page"]
-
 results = []
 
-for pkg in page:
-  # if python version is specified, filter only package files
-  # that contains the python version substring (e.g. py310)
-  if args.python:
-    if py_str in pkg["filename"]:
+if resp_json["data"]["package"]:
+  page = resp_json["data"]["package"]["variants"]["page"]
+
+
+  for pkg in page:
+    # if python version is specified, filter only package files
+    # that contains the python version substring (e.g. py310)
+    if args.python:
+      if py_str in pkg["filename"]:
+        results.append(pkg["filename"])
+    else:
       results.append(pkg["filename"])
-  else:
-    results.append(pkg["filename"])
 
 print(json.dumps(results))
